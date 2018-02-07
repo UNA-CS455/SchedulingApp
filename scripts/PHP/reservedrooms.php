877cbf6137ae530a682ba407019b1cb3d3d1ca6d
adrionder <35594258+adrionder@users.noreply.github.com>
2018-02-07 16:33:40 -0600

    Update reservedrooms.php

--- scripts/PHP/reservedrooms.php
+++ scripts/PHP/reservedrooms.php
@@ -1,4 +1,9 @@
 <?php session_start();
+	/*if (!isset($_SESSION['username'])) {
+		header("location:index.php");
+		exit;
+	}*/
+
     $servername = "localhost";
     $username = "root";
     $password = "";
