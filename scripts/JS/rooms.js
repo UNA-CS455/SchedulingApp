ae4585df38145194140b69c6dba25362fb86da08
dillonborden21 <dillon_borden18@yahoo.com>
2018-02-12 18:45:34 -0600

    Rooms View & Main Page Layout Ready (2/12/18)

---
+++ scripts/JS/rooms.js
@@ -0,0 +1,104 @@
+var showrooms = false;
+
+var roomSelected = null;
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+	document.getElementById("roominsert").innerHTML = this.responseText;
+   }
+};
+xhttp.open("GET", "scripts/PHP/allrooms.php", true);
+xhttp.send();
+
+function toggleRoomView()
+{
+	if (showrooms == false)
+	{
+		showrooms = true;
+		document.getElementById('roomMenu').style.display = "inline";
+		document.getElementById('roomSelect').style.top = "88%";
+		document.getElementById('roomWrapper').style.display = "none";
+		document.getElementById('selectedRoom').style.top = "88%";
+	}
+	else
+	{
+		showrooms = false;
+		document.getElementById('roomMenu').style.display = "none";
+		document.getElementById('roomSelect').style.top = "13%";
+		document.getElementById('roomWrapper').style.display = "inline";
+		document.getElementById('selectedRoom').style.top = "14%";
+	}
+}
+
+function selectRoom(id){
+	document.getElementById('selectedRoom').innerHTML = "<b>" + id + "</b>";
+	console.log(roomSelected);
+	if (roomSelected != null){
+		document.getElementById(roomSelected).style.backgroundColor = "white";
+	}
+	roomSelected = id;
+	document.getElementById(id).style.backgroundColor = "gray";
+	showrooms = false;
+	document.getElementById('roomMenu').style.display = "none";
+	document.getElementById('roomSelect').style.top = "13%";
+	document.getElementById('roomWrapper').style.display = "inline";
+	document.getElementById('selectedRoom').style.top = "14%";
+
+	// HERE IS WHERE AJAX CALL TO MAIN CALENDAR GOES!
+}
+
+/*
+	A function whose purpose is to populate the agenda table with the user's
+	current reservations with a given email address.
+*/
+function getAgendaReservations() {
+
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var reservation = JSON.parse(this.responseText);
+			var runString = "<h2>Your Active Reservations</h2>";
+			runString+= "<br><table id = 'agendaTable' >\n<th>Room</th><th>From</th><th>To</th><th>Details</th>\n";
+			for(var i = 0; i < reservation.length; i++){
+				runString += "<tr>\n";
+				runString += "<td>" + reservation[i].roomnumber + "</td>";
+				runString += "<td>" + reservation[i].start + "</td>";
+				runString += "<td>" + reservation[i].end + "</td>";
+				runString += "<td><a href =" + "TODO" + ">Edit</a><br><a href =" + "TODO" + ">Remove</a></td>";
+				runString += "</tr>\n"; 
+			}
+			runString += "</table>";
+			document.getElementById("agenda").innerHTML += runString;
+		}
+	};
+	xmlhttp.open("GET", "res_user.php", true);
+	xmlhttp.send();
+
+    
+}
+
+function typeChanged(id){
+	if (id == "Any"){
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("roominsert").innerHTML = this.responseText;
+		}
+		};
+		xhttp.open("GET", "scripts/PHP/allrooms.php", true);
+		xhttp.send();
+	}
+	else{
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById("roominsert").innerHTML = this.responseText;
+			}
+		};
+		xhttp.open("GET", "scripts/PHP/roombytype.php?type=" + id, true);
+		xhttp.send();
+	}
+	
+
+}
---
+++ scripts/PHP/allrooms.php
@@ -0,0 +1,51 @@
+<?php session_start();
+	/*if (!isset($_SESSION['username'])) {
+		header("location:index.php");
+		exit;
+	}*/
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cs455";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
+
+<?php
+	$sql = "SELECT * FROM rooms ORDER BY roomid";
+    $result = $conn->query($sql);
+	$i = 0;
+
+	$firstPrinted = false;
+	while ($row = $result->fetch_assoc()) 
+	{
+		if($i == 0)
+		{
+			echo "<tr>";
+		}
+		if ($firstPrinted == false){
+			$firstPrinted = true;
+			echo "<td onclick='selectRoom(this.id)' class='room' id='No Room Preference/All'><font class='roomText'><b>All Rooms/No Preference</b><br><br></font></td>";
+			$i++;
+		}
+		if($i == 6)
+		{
+			echo "<td onclick='selectRoom(this.id)' class = 'room' id = '".$row['roomid']."'><font class='roomText'><b>" . $row['roomid'] ."</b><br>". $row['seats'] ."<br>" . $row['type'] . "</font></td>";
+			echo "</tr>";
+			$i = 0;
+		}
+		
+		else
+		{	
+			echo "<td onclick='selectRoom(this.id)' class = 'room' id = '".$row['roomid']."'><font class='roomText'><b>" . $row['roomid'] ."</b><br>". $row['seats'] ."<br>" . $row['type'] . "</font></td>";
+			$i++;
+		}
+		
+
+		
+	}
+
+$conn->close();
+?>
