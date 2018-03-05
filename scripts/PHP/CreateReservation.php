a4ecf243cb87caa1c98ddc0f53075dc2c897aacb
Derek Brown <derekcb@live.com>
2018-03-05 17:14:33 -0600

    update to support new time selection

--- scripts/PHP/CreateReservation.php
+++ scripts/PHP/CreateReservation.php
@@ -15,70 +15,6 @@
 
 	processReservation();
 
-//-----------------------------------------------------------------------------
-//Function to display reservation form once a day is clicked in index.html
-//-----------------------------------------------------------------------------
-function displayReservationForm()
-{
-
-/*echo <<<HTMLBLOCK
-<!DOCTYPE html>
-<html>
-<head>
-<title>UNA Scheduling app</title>
-</head>
-<body>
-
-<h1>Make Reservation</h1>
-<form method="POST" action="CreateReservation.php">
-  Reserving email*:
-  <input type="email" name="owneremail"><br>
-
-  Room Number:
-  <input type="text" name="roomnumber"><br>
-
-  <p>Duration*:</p>
-  Start time:
-  <input type="text" name="starthour" style="width: 48px">
-  <input type="text" name="startminute" style="width: 48px">
-  <select name="start">
-  <option value="AM">AM</option>
-  <option value="PM">PM</option>
-  </select>
-  <input id="date" type="date" name="startdate" placeholder="2018/01/26" required/><br>
-
-  End time:
-  <input type="text" name="endhour" style="width: 48px">
-  <input type="text" name="endminute" style="width: 48px">
-  <select name="end">
-  <option value="AM">AM</option>
-  <option value="PM">PM</option>
-  </select>
-  <input id="date" type="date" name="enddate" placeholder="2018/01/26" required/><br>
-
-  Recurring:
-  <select name="occur">
-  <option value="Once">Just Once</option>
-  <option value="Weekly">Weekly</option>
-  <option value="Monthly">Monthly</option>
-</select><br>
-
-  <input type="checkbox" name="allowshare" value="1">Allow room sharing<br>
-
-  Expected number of seats needed:
-  <input type="text" name="numberOfSeats" style="width: 48px"><br>
-
-  Comments<br>
-  <input type = "text" textarea rows="10" cols="50" name="comment">
-  </textarea><br>
-
-  <button type="submit" name="reserve" value="Submit">Make reservation</button>
-</form>
-
-</body>
-</html>
-HTMLBLOCK;*/
-}
 
 //-------------------------------------------------------------------------------------------------------------
 //Function to process the reservation into the database once reserve is clicked
@@ -143,12 +79,6 @@ $endminute = filter_var($endminute, FILTER_VALIDATE_REGEXP, array("options"=>arr
 //variable for end date of reservation
 $enddate = ($_POST['enddate']);
 
-//dropdown menu for start AM or PM select option
-$start = ($_POST['start']);
-
-//dropdown menu for end AM or PM select option
-$end = ($_POST['end']);
-
 //dropdown menu for how often the reservation should occur
 $occur = ($_POST['occur']);
 
@@ -171,11 +101,11 @@ if ($conn->connect_error) {
 }
 
 //if connection is success, insert data into database and echo to user result
-$sql = "INSERT INTO reservations (roomnumber, owneremail, allowshare, headcount, startdate, enddate, starttime, endtime, occur, comment, res_email) VALUES ('$roomnumber', '$owneremail', '$allowshare', '$numberOfSeats', '$startdate', '$enddate', '$starthour:$startminute $start', '$endhour:$endminute $end', '$occur', '$comment', '$logged_in_user')";
+$sql = "INSERT INTO reservations (roomnumber, owneremail, allowshare, headcount, startdate, enddate, starttime, endtime, occur, comment, res_email) VALUES ('$roomnumber', '$owneremail', '$allowshare', '$numberOfSeats', '$startdate', '$enddate', '$starthour:$startminute', '$endhour:$endminute', '$occur', '$comment', '$logged_in_user')";
 
 	if ($conn->query($sql) === TRUE) {
                 echo "Reservation made successfully";
-		include 'mail.php';
+		//include 'mail.php'; uncomment when on deployed version
             } else {
                 echo "Error making reservation: " . $conn->error;
             }
