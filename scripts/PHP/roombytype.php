75a5971d37c7638a9c61d2c6fac66eb6ad42925b
dillonborden21 <33707513+dillonborden21@users.noreply.github.com>
2018-03-05 17:25:54 -0600

    Favorites Implemented

--- scripts/PHP/roombytype.php
+++ scripts/PHP/roombytype.php
@@ -1,8 +1,7 @@
 <?php session_start();
-	/*if (!isset($_SESSION['username'])) {
-		header("location:index.php");
-		exit;
-	}*/
+	if (!isset($_SESSION['username'])){
+			$_SESSION['username'] = "dbrown4@una.edu";
+	}
 	require "db_conf.php"; // set servername,username,password,and dbname
 	
     $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,6 +11,54 @@
 ?>
 
 <?php
+
+	$sql = "SELECT * FROM favorites WHERE email='" . $_SESSION['username'] . "'";
+    $result = $conn->query($sql);
+	$i = 0;
+	$headerPrinted = false;
+	while ($row = $result->fetch_assoc()) 
+	{
+		if ($headerPrinted == false){
+			echo "<tr style='height: 7%;'><th style='font-size: 3vmin;'>Favorites</th></tr>";
+			$headerPrinted = true;
+		}
+		$imgName = "images/fav-unselect.png";
+		if($i == 0)
+		{
+			echo "<tr>";
+		}
+		if($i == 6)
+		{
+			$sql = "SELECT * FROM favorites WHERE roomid='" . $row['roomid'] . "' AND email='" . $_SESSION['username'] . "'";
+			$result2 = $conn->query($sql);
+
+			while ($row2 = $result2->fetch_assoc()) {
+				// there is a favorite.
+				$imgName = "images/fav-select.png";
+			}
+			echo "<td class = 'room' id = '".$row['roomid']."'><img src='" . $imgName . "' onclick='favoriteClicked(this.parentElement);' class='favoriteIcon'><font id = '".$row['roomid']."' onclick='selectRoom(this.id)' class='roomText'><b></font></td>";
+			echo "</tr>";
+			$i = 0;
+		}
+		
+		else
+		{	
+			$sql = "SELECT * FROM favorites WHERE roomid='" . $row['roomid'] . "' AND email='" . $_SESSION['username'] . "'";
+			$result2 = $conn->query($sql);
+
+			while ($row2 = $result2->fetch_assoc()) {
+				// there is a favorite.
+				$imgName = "images/fav-select.png";
+			}
+			echo "<td class = 'room' id = '".$row['roomid']."'><img src='" . $imgName . "' onclick='favoriteClicked(this.parentElement);' class='favoriteIcon'><font id = '".$row['roomid']."' onclick='selectRoom(this.id)' class='roomText'><b>" . $row['roomid'] ."</b><br></font></td>";
+			$i++;
+		}
+		
+
+		
+	}
+	
+
 	if ($_GET['type'] == "Any"){
 		if ($_GET['comp'] == "Any"){
 			$sql = "SELECT * FROM rooms ORDER BY roomid";
@@ -40,9 +87,15 @@
     $result = $conn->query($sql);
 	$i = 0;
 
+	$headerPrinted = false;
 	$firstPrinted = false;
 	while ($row = $result->fetch_assoc()) 
 	{
+		if ($headerPrinted == false){
+			echo "<tr style='height: 7%;'><th style='font-size: 3vmin;'>All Rooms</th></tr>";
+			$headerPrinted = true;
+		}
+		$imgName = "images/fav-unselect.png";
 		if ($_GET['floor'] == "first"){
 			if ($row['floor']!= "1"){
 				continue;
@@ -72,14 +125,28 @@
 		}
 		if($i == 6)
 		{
-			echo "<td onclick='selectRoom(this.id)' class = 'room' id = '".$row['roomid']."'><font class='roomText'><b>" . $row['roomid'] ."</b><br>". $row['seats'] ."<br>" . $row['type'] . "</font></td>";
+			$sql = "SELECT * FROM favorites WHERE roomid='" . $row['roomid'] . "' AND email='" . $_SESSION['username'] . "'";
+			$result2 = $conn->query($sql);
+
+			while ($row2 = $result2->fetch_assoc()) {
+				// there is a favorite.
+				$imgName = "images/fav-select.png";
+			}
+			echo "<td class = 'room' id = '".$row['roomid']."'><img src='" . $imgName . "' onclick='favoriteClicked(this.parentElement);' class='favoriteIcon'><font id = '".$row['roomid']."' onclick='selectRoom(this.id)' class='roomText'><b>" . $row['roomid'] ."</b><br>". $row['seats'] ."<br>" . $row['type'] . "</font></td>";
 			echo "</tr>";
 			$i = 0;
 		}
 		
 		else
 		{	
-			echo "<td onclick='selectRoom(this.id)' class = 'room' id = '".$row['roomid']."'><font class='roomText'><b>" . $row['roomid'] ."</b><br>". $row['seats'] ."<br>" . $row['type'] . "</font></td>";
+			$sql = "SELECT * FROM favorites WHERE roomid='" . $row['roomid'] . "' AND email='" . $_SESSION['username'] . "'";
+			$result2 = $conn->query($sql);
+
+			while ($row2 = $result2->fetch_assoc()) {
+				// there is a favorite.
+				$imgName = "images/fav-select.png";
+			}
+			echo "<td class = 'room' id = '".$row['roomid']."'><img src='" . $imgName . "' onclick='favoriteClicked(this.parentElement);' class='favoriteIcon'><font id = '".$row['roomid']."' onclick='selectRoom(this.id)' class='roomText'><b>" . $row['roomid'] ."</b><br>". $row['seats'] ."<br>" . $row['type'] . "</font></td>";
 			$i++;
 		}
 		
