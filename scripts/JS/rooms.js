7242e496c8a906a59ccb100cdb829317537c176b
dillonborden21 <33707513+dillonborden21@users.noreply.github.com>
2018-04-04 14:47:24 -0500

    Quick Fixes

--- scripts/JS/rooms.js
+++ scripts/JS/rooms.js
@@ -629,7 +629,7 @@ function logoutUser(){
 		window.location.href = 'logout.php';
 	}
 	else{
-		window.location.href = '/scripts/PHP/logout.php';
+		window.location.href += 'scripts/PHP/logout.php';
 	}
 }
 
