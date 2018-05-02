b027d4eac2f00640b2175d43dd7ef30810f9662c
jbrazier456 <35587201+jbrazier456@users.noreply.github.com>
2018-05-01 20:21:15 -0500

    Add files via upload

--- scripts/PHP/updateUser.php
+++ scripts/PHP/updateUser.php
@@ -31,7 +31,7 @@
 				. $row['roomnumber'] . "</td><td>" . $row['allowshare'] . "</td><td>" . $row['headcount'] . "</td><td>"
 				. $row['startdate'] . "</td><td>" . $row['enddate'] . "</td><td>" . $row['starttime'] . "</td><td>"
 				. $row['endtime'] . "</td><td>" . $row['occur'] . "</td>";
-			echo "<td><form action=room_remove.php METHOD='POST'>";
+			echo "<td><form action=room_remove_user.php METHOD='POST'>";
 			echo "<input type='text' value='" . $row['id'] . "' name='id' hidden>";
 			echo "<input type='submit' value='Delete'>";
 			echo "</form></td></tr>";
