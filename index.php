38b29a151ea1e30a79b8a5cd61a893810caf5f5a
unknown <fglover@una.edu>
2018-06-15 08:26:26 -0500

    Removed testing comment

--- index.php
+++ index.php
@@ -14,8 +14,6 @@
 ?>
     <html>
 
-    <!-- quint making another change -->
-
     <head>
         <!-- Include Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
