370b73efbb4f2690dbbb8c6050922e8b46a18a52
unknown <fglover@una.edu>
2018-08-16 22:22:43 -0500

    Rewrote delete user calls

--- scripts/PHP/deleteUser.php
+++ scripts/PHP/deleteUser.php
@@ -1,15 +1,33 @@
 <?php
-	$user = $_POST['user'];
+	// $user = $_POST['user'];
 	require "db_conf.php";
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
-	$sql = "DELETE FROM users WHERE email='$user' LIMIT 1";
-	if ($conn->query($sql) === TRUE) {
-			echo "Record deleted successfully";
-		} else {
-			echo "Error deleting record: " . $conn->error;
-		}
+	// $sql = "DELETE FROM users WHERE email='$user' LIMIT 1";
+	// if ($conn->query($sql) === TRUE) {
+	// 		echo "Record deleted successfully";
+	// 	} else {
+	// 		echo "Error deleting record: " . $conn->error;
+	// 	}
 
-    $conn->close();
+  //DELETE USER=======================================================
+  // If deleteUser.php gets the users email
+  $email = $_GET['email'];
+  if(isset($_GET['email']))
+  {
+    $sql = "DELETE FROM `users` WHERE `email`='$email'";
+
+    if($conn->query($sql) === TRUE)
+    {
+      header('Location: ' .$_SERVER['HTTP_REFERER']);
+    }
+    else
+    {
+      echo "failed";
+    }
+  }
+
+  $conn->close();
+?>
