82de7289973f0d81143da0cebf9b5739d0dca076
unknown <fglover@una.edu>
2018-10-11 10:23:19 -0500

    Finished sql calls

--- scripts/PHP/favorite.php
+++ scripts/PHP/favorite.php
@@ -6,6 +6,7 @@ if ($_GET['room']){
 
   $gotRoom = $_GET['room'];
   $email = $_SESSION['username'];
+
   // add a favorite
   $conn = new mysqli($servername, $username, $password, $dbname);
   // Check connection
@@ -13,27 +14,35 @@ if ($_GET['room']){
     die("Connection failed: " . $conn->connect_error);
   } 
 
-  $checkSql = "Select * FROM favorites WHERE roomid = $gotRoom AND email = $email";
-  var_dump( $conn->query($checkSql));
-  if(!$conn->query($checkSql))
+  $checkSql = "Select * FROM favorites WHERE roomid = '$gotRoom' AND email = '$email'";
+
+  $checkResponse = $conn->query($checkSql);
+
+  $values = $checkResponse->fetch_all();
+
+
+
+  var_dump($values);
+
+  if(!$values > 0)
   {
-    echo "added a room";
+    // echo "added a room";
     $sql = "INSERT INTO favorites (email, roomid)
     VALUES ('" . $_SESSION['username'] . "', '" . $_GET['room'] . "')";
 
     if ($conn->query($sql) === TRUE) {
       echo "New record created successfully";
-      // header("Location: " . $_SERVER['HTTP_REFERER']);
+      header("Location: " . $_SERVER['HTTP_REFERER']);
     } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
     }
 
     $conn->close();
   }
-  elseif($conn->query($checkSql))
+  else
   {
-    echo "deleted a room";
-    // remove one.
+    // echo "deleted a room";
+    // // remove one.
     $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
@@ -41,11 +50,13 @@ if ($_GET['room']){
     } 
 
     // sql to delete a record
-    $sql = "DELETE FROM favorites WHERE email= $email AND roomid= $gotRoom LIMIT 1";
+    $sql = "DELETE FROM favorites WHERE email= '$email' AND roomid= '$gotRoom' LIMIT 1";
+
+    var_dump($sql);
 
     if ($conn->query($sql) === TRUE) {
       echo "Record deleted successfully";
-      // header("Location: " . $_SERVER['HTTP_REFERER']);
+      header("Location: " . $_SERVER['HTTP_REFERER']);
     } else {
       echo "Error deleting record: " . $conn->error;
     }
@@ -53,25 +64,6 @@ if ($_GET['room']){
     $conn->close();
   }
 }
-
-  // $conn = new mysqli($servername, $username, $password, $dbname);
-
-  // if($conn->connect_error)
-  // {
-  //   die("Connection Failed: " . $conn->connect_error);
-  // }
-
-  // if($_GET['room'])
-  // {
-  //   $gotRoom = $_GET['room'];
-
-  //   $checkSql = "Select * FROM favorites WHERE roomid = $gotRoom AND email = '".$_SESSION['username']"'";
-
-  // }
-  // else
-  // {
-  //   echo "Something went wrong";
-  // }
 
 ?>
 
