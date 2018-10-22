bc67369c21d3f756c97dcbf09dfd3ff1f7a4d7fc
quintg1991 <quintg1991@gmail.com>
2018-10-22 00:04:44 -0500

    Played around with editclicked. Didn't change anything

--- scripts/JS/rooms.js
+++ scripts/JS/rooms.js
@@ -1226,6 +1226,7 @@ function updateClicked()
 function editClicked(ele)
 {
 	var id = String(ele.children[0].id);
+
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200)
