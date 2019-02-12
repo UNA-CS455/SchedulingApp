8a0c52fb28685e9cf8aee99bbd3cdd35da0e7731
Hayden Cornelius <haydenc52@gmail.com>
2019-02-12 21:01:43 +0000

    Making Number of computers look good

--- scripts/PHP/createEditRoom.php
+++ scripts/PHP/createEditRoom.php
@@ -232,9 +232,10 @@ if (isset ( $_POST ['submit'] ))
 						<button class="btn btn-secondary" id="submit" name="submit" type="submit"><?php (($beingEdited == true) ? print "Edit Room" : print "Create Room") ?></button>
 					</div>
                     <div class="col-md-2 form-group" style="<?php echo($roomToEdit['hascomputers'] == 1) ? "display: run-in" : "display: none" ?>">
-						<label for="numComputers">Number of Computers</label>
-						<input type="text" id="numComputers" name="numcomputers"
-							class="form-control">
+                    	<b>Number of Computers</b>
+                    	<div class="col-md-2">
+							<input type="text" id="numComputers" name="numcomputers" class="form-control">
+						</div>
 					</div>
 					<div class="col-lg-1" id="allowedReserve" style="margin: 1px; min-width: 250px; <?php echo ($roomToEdit['limit'] == 1) ? "display: run-in" : "display: none" ?>">
 						<b>Allowed Users</b>
