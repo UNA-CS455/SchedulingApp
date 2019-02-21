fd23c607b567938704b834ccb8723872946a6a23
Hayden Cornelius <haydenc52@gmail.com>
2019-02-21 15:46:21 +0000

    Testing hide/show again

--- scripts/PHP/createEditRoom.php
+++ scripts/PHP/createEditRoom.php
@@ -250,7 +250,7 @@ if(isset($_POST['addUser'])){
 						</div>
 					</div>
 					<div class="col-xl-3" >
-							<button class="btn btn-secondary" style="margin-top: 55px; margin-left: 50px; <?php// echo ($roomToEdit['limit'] == 1) ? "display: run-in" : "display: none"?>" 
+							<button class="btn btn-secondary" style="margin-top: 55px; margin-left: 50px <?php// echo ($roomToEdit['limit'] == 1) ? "display: run-in" : "display: none"?>" 
 								id="addUser" name="addUser" type="submit">Add User</button>
 					</div>
 					<div>
@@ -271,7 +271,7 @@ if(isset($_POST['addUser'])){
                     	<b>Spacer, shouldn't be seen</b>
 					</div>
 					
-					<div class="col-lg-1" id="allowedReserve" style="margin: 1px; min-width: 250px; <?php// echo ($roomToEdit['limit'] == 1) ? "display: run-in" : "display: none" ?>">
+					<div class="col-lg-1" id="allowedReserve" style="margin: 1px; min-width: 250px <?php// echo ($roomToEdit['limit'] == 1) ? "display: run-in" : "display: none" ?>">
 						<b>Allowed Users</b>
 						<div class="col-md-2" style="overflow-y:auto; min-height: 96px; min-width: 250px; max-height: 96px; border: 1px solid black">
 							<table>
