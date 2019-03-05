6de7a8bc6ed1bff8511cbcb03e4ddaef7048442a
Hayden Cornelius <haydenc52@gmail.com>
2019-03-05 23:12:54 +0000

    Testing javascript stuff

--- scripts/JS/limitUsers.js
+++ scripts/JS/limitUsers.js
@@ -7,13 +7,15 @@
 
 
 
-function openConfirmCreateUser(name)
+function openConfirmCreateUser(name, roomid)
 {
 	// xhttp send is structured like this:
 		// xhttp.send("variable=" + variable + "&variable2=" + variable2 + ...)
 
     var buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='insertUserWhitelist()'>Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
-	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name, "Add user", buttonhtml, false);
+	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name + " " + roomid, "Add user", buttonhtml, false);
+	
+	
 	
 	xhttp.open("POST", "scripts/PHP/addUserWhitelist.php", true);
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
