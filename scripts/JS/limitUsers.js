f69fa48d6f863ff5d05d3df3225e2177380cbbc2
Hayden Cornelius <haydenc52@gmail.com>
2019-03-05 23:23:55 +0000

    Testing javascript stuff

--- scripts/JS/limitUsers.js
+++ scripts/JS/limitUsers.js
@@ -13,11 +13,11 @@ function openConfirmCreateUser(name, roomid)
 		// xhttp.send("variable=" + variable + "&variable2=" + variable2 + ...)
 
     var buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='insertUserWhitelist()'>Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
-	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name + " in room " + roomid, "Add user", buttonhtml, false);
+	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name, "Add user", buttonhtml, false);
 	
 	
 	
 	xhttp.open("POST", "scripts/PHP/addUserWhitelist.php", true);
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xhttp.send();// send stuff
+	xhttp.send("allowedUser=" + name + "&roomid=" + roomid);// send stuff
 }
--- scripts/PHP/addUserWhitelist.php
+++ scripts/PHP/addUserWhitelist.php
@@ -3,17 +3,16 @@
 require "db_conf.php";
 $conn = new mysqli ( $servername, $username, $password, $dbname ) or die( "Connection failed: " . $conn->connect_error );
 
+$email = $_POST['allowedUser'];
+$roomid = $_POST['roomid'];
+0
+$_allowedSql = "INSERT INTO `whitelist` (`email`, `roomid`) VALUES ('$email', '$roomid')";
+$conn->query ( $_allowedSql );
 
+if(!empty($conn->error)){
+    echo $conn->error;
+}
 
-
-
-
-
-
-
-
-
-
-
+$conn->close;
 
 ?>
