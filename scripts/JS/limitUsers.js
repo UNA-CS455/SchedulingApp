222e9f1511654b07593d6145b2d6c9d9c973af55
Hayden Cornelius <haydenc52@gmail.com>
2019-03-05 01:27:32 +0000

    Testing scope of .js file

--- scripts/JS/limitUsers.js
+++ scripts/JS/limitUsers.js
@@ -29,6 +29,6 @@ function openConfirmCreateUser(name)
 
 // 	buttonhtml = "<button class='modal-button' onclick='closeModal()'>Ok</button>"; //this was missing a semicolon? unsure why, added it back in.
     buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='insertUserWhitelist()'>Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
-	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name + buttonhtml, "Add user", "", false);
+	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name, "Add user", buttonhtml, false);
 
 }
