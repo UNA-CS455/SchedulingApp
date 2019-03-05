de5b4bf1c52874bc5028f2e5b639ae34f706370e
Hayden Cornelius <haydenc52@gmail.com>
2019-03-05 17:31:02 +0000

    Testing the js function openConfirmCreateUser with stuff

--- scripts/JS/limitUsers.js
+++ scripts/JS/limitUsers.js
@@ -7,31 +7,16 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function openConfirmCreateUser(asdf)
+function openConfirmCreateUser(name)
 {
 	// xhttp send is structured like this:
 		// xhttp.send("variable=" + variable + "&variable2=" + variable2 + ...)
 		
 	//var name = data.allowedUser;
 	//var name = document.getElementById(asdf).value;
-	var name = asdf.value;
+	//var name = asdf.value;
 	
-    buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='insertUserWhitelist()'>Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
+    var buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='insertUserWhitelist()'>Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
 	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name, "Add user", buttonhtml, false);
 	
 	xhttp.open("POST", "scripts/PHP/addUserWhitelist.php", true);
