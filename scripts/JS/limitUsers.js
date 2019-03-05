26b34991f451820ae1fe665b9f55192960ed0b50
Hayden Cornelius <haydenc52@gmail.com>
2019-03-05 16:37:46 +0000

    Testing the js function openConfirmCreateUser with 'data' parameter

--- scripts/JS/limitUsers.js
+++ scripts/JS/limitUsers.js
@@ -22,13 +22,17 @@
 
 
 
-function openConfirmCreateUser(name)
+function openConfirmCreateUser(data)
 {
-	// first display the modal using display: block (by default should be none)
-	// set id of info tag to append this info below.
-
-
-// 	buttonhtml = "<button class='modal-button' onclick='closeModal()'>Ok</button>"; //this was missing a semicolon? unsure why, added it back in.
+	// xhttp send is structured like this:
+		// xhttp.send("variable=" + variable + "&variable2=" + variable2 + ...)
+		
+	var name = data.allowedUser;
+	
     buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='insertUserWhitelist()'>Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
 	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name, "Add user", buttonhtml, false);
+	
+	xhttp.open("POST", "scripts/PHP/addUserWhitelist.php", true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send();// send stuff
 }
