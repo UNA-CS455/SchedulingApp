78da9ecd38d9868328b6b0ae499e1cf0b189f972
Hayden Cornelius <haydenc52@gmail.com>
2019-03-05 17:32:08 +0000

    Testing the js function openConfirmCreateUser with stuff

--- scripts/JS/limitUsers.js
+++ scripts/JS/limitUsers.js
@@ -7,14 +7,14 @@
 
 
 
-function openConfirmCreateUser(name)
+function openConfirmCreateUser(formElement)
 {
 	// xhttp send is structured like this:
 		// xhttp.send("variable=" + variable + "&variable2=" + variable2 + ...)
 		
 	//var name = data.allowedUser;
 	//var name = document.getElementById(asdf).value;
-	//var name = asdf.value;
+	var name = formElement.value;
 	
     var buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='insertUserWhitelist()'>Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
 	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name, "Add user", buttonhtml, false);
