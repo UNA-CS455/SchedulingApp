37254b2cc5f0a3967b79c09a2d9d71736223c70b
Hayden Cornelius <haydenc52@gmail.com>
2019-03-05 01:48:05 +0000

    Testing scope of .js file

--- scripts/JS/limitUsers.js
+++ scripts/JS/limitUsers.js
@@ -27,8 +27,15 @@ function openConfirmCreateUser(name)
 	// first display the modal using display: block (by default should be none)
 	// set id of info tag to append this info below.
 
+
 // 	buttonhtml = "<button class='modal-button' onclick='closeModal()'>Ok</button>"; //this was missing a semicolon? unsure why, added it back in.
     buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='insertUserWhitelist()'>Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
 	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name, "Add user", buttonhtml, false);
+	sleep(5000).then(() => {
+  
+    })
+}
 
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
