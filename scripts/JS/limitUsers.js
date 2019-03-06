ad2887598112382fbf515b3e50af5e01c96cfe99
Hayden Cornelius <haydenc52@gmail.com>
2019-03-06 17:27:53 +0000

    Testing stuff

--- scripts/JS/limitUsers.js
+++ scripts/JS/limitUsers.js
@@ -12,10 +12,15 @@ function openConfirmCreateUser(name, roomid)
 	// xhttp send is structured like this:
 		// xhttp.send("variable=" + variable + "&variable2=" + variable2 + ...)
 
-    // var buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='addToWhiteList(" + name + "," + roomid + ")' >Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
-    var buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='addToWhiteList('user@una.edu', 'Keller 233')' >Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
-
+    var buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL'' >Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
+    // var buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='addToWhiteList('user@una.edu', 'Keller 233')' >Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
 	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name, "Add user", buttonhtml, false);
+	
+	document.getElementById('yesAddWL').onclick = function() {
+		closeModal();
+		addToWhiteList(name, roomid);
+	};
+	
 	
 	
 }
