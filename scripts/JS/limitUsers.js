fb875eec7803b04939aafbd0ebb565830fe557b0
Hayden Cornelius <haydenc52@gmail.com>
2019-03-06 19:01:06 +0000

    Fixing saveChanges

--- scripts/JS/limitUsers.js
+++ scripts/JS/limitUsers.js
@@ -56,7 +56,7 @@ function openConfirmCreateUser(name, roomid, beingEdited)
 	document.getElementById('yesAddWL').onclick = function() {
 		closeModal();
 		// alert(roomid + roomType + floorNum + seats + numComputers + limit + beingEdited);
-		alert(document.getElementById('hasComputersCheck').value + ' ' + hasComputers + ' ' + beingEdited);
+		alert('hasComputersCheck= ' + document.getElementById('hasComputersCheck').value + ' hasComputers=' + hasComputers + ' beingEdited=' + beingEdited + ' limit= ' + limit);
 		addWL(name, roomid);
 		saveChanges(roomid, roomType, floorNum, seats, numComputers, limit, beingEdited, hasComputers);
 		
