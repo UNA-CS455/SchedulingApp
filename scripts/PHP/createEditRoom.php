909b4f9f39aa5e24c283bb4050cd46ba183e2ce3
Hayden Cornelius <haydenc52@gmail.com>
2019-03-12 18:59:07 +0000

    Testing verifyUserExists for onclick

--- scripts/PHP/createEditRoom.php
+++ scripts/PHP/createEditRoom.php
@@ -299,7 +299,7 @@ function verifyUserExists($userToVerify){
 					<div class="col-xl-3" >
 							<button class="btn btn-secondary" style="margin-top: 55px; margin-left: 50px; <?php echo ($roomToEdit['limit'] == 1) ? "display: run-in" : "display: none"?>" 
 								id="addUser" name="addUser" type="button" 
-								onclick="openConfirmAddUser(<?php echo(verifyUserExists($roomToEdit['allowedUser'])); ?>, document.getElementById('allowedUser').value, document.getElementById('roomid').value, <?php echo(json_encode($beingEdited)); ?>)" >Add User</button>
+								onclick="openConfirmAddUser(<?php //echo(verifyUserExists($roomToEdit['allowedUser'])); ?>, document.getElementById('allowedUser').value, document.getElementById('roomid').value, <?php echo(json_encode($beingEdited)); ?>)" >Add User</button>
 					</div>
 					<!--<div button class="btn btn-secondary" style="margin-top: 55px; margin-left: 50px"-->
 					<!--			id="deleteUser" name="deleteUser" type="submit">Delete User</button>-->
