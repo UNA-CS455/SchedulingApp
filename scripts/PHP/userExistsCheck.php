83416ce706c32c1320ad07431d5d5e645709cf41
Hayden Cornelius <haydenc52@gmail.com>
2019-03-16 17:46:44 +0000

    Added a semicolon to the sql

--- scripts/PHP/userExistsCheck.php
+++ scripts/PHP/userExistsCheck.php
@@ -1,7 +1,7 @@
 <?php session_start();
 
 require "db_conf.php";
-$conn = new mysqli ( $servername, $username, $password, $dbname );
+$conn = new mysqli ($servername, $username, $password, $dbname );
 if ($conn->connect_error)
 {
 	die ( "Connection failed: " . $conn->connect_error );
@@ -10,7 +10,7 @@ if ($conn->connect_error)
 $userToVerify = $_POST['allowedUser'];
 
 
-$verifySql = "SELECT * FROM `users` WHERE `users`.`email` = $userToVerify";
+$verifySql = "SELECT * FROM `users` WHERE `users`.`email` = $userToVerify;";
 
 if($verifyRes = $conn->query($verifySql)){
     if($verifyRes->num_rows == 1){
