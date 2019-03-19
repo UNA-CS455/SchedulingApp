68426f142338d3bdf0956c7579c09a3bf06eac16
Hayden Cornelius <haydenc52@gmail.com>
2019-03-19 16:30:02 +0000

    Testing if we can get rid of a line of code

--- scripts/PHP/createEditRoom.php
+++ scripts/PHP/createEditRoom.php
@@ -259,7 +259,7 @@ function saveChanges($conn, $beingEdited, $post_vars){
 						<div class="row-xs-0 form-group" id="computerBlock" style="min-width: 190px; <?php echo ($roomToEdit['hascomputers'] == 1) ? "display: run-in" : "display: none"?>">
 	                    	<b>Number of Computers</b>
 	                    	<div class="row-md-2" style="max-width: 175px">
-								<input type="text" id="numComputers" name="numcomputers" class="form-control" <?php echo($roomToEdit['numcomputers']) ? 'value=" '.$roomToEdit['numcomputers'].' "' : '' ?>>
+								<input type="text" id="numComputers" name="numcomputers" class="form-control" <?php echo($roomToEdit['numcomputers']) ? 'value=" '.$roomToEdit['numcomputers'].' "' : 'value="0"' ?>>
 							</div>
 						</div>
 						
