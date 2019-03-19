40203379478029cc7be9d7cb737049e18b98333d
Hayden Cornelius <haydenc52@gmail.com>
2019-03-19 00:08:39 +0000

    Passed saveObject to saveChanges

--- scripts/JS/limitUsers.js
+++ scripts/JS/limitUsers.js
@@ -42,6 +42,18 @@ function openConfirmAddUser(name, roomid, beingEdited)
 	var roomType = document.getElementById('type').value;
 	var floorNum = document.getElementById('floor').value;
 	var seats = document.getElementById('seats').value;
+	
+	var saveObject = {
+		numComp : numComputers,
+		hasComp : hasComputers,
+		lim : limit,
+		seatNum : seats,
+		floor : floorNum,
+		roomT : roomType,
+		roomID : roomid,
+		edit : beingEdited
+	};
+	
 
     var buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' >Yes</button> <button class='modal-button btn btn-danger' id='noAddWL' onclick='closeModal()'>No</button>";
 	showMessageBox("<br><br>Are you sure you want to add:<br><br>" + name, "Add user", buttonhtml, false);
@@ -49,60 +61,12 @@ function openConfirmAddUser(name, roomid, beingEdited)
 	
 	document.getElementById('yesAddWL').onclick = function() {
 		closeModal();
-		addWL(name, roomid);
+		addWL(name, roomid, saveObject);
 		
 		//save changes
 	};
 
 }
-
-
-
-
-// //************************************************************************************************
-// // Method Name: checkUserExists
-// //
-// // Incoming params: name (String)
-// //
-// // Purpose: Checks if the user passed in actually exists in the system. Makes a call to a PHP
-// //			file called userExistsCheck.php, which actually verifies the user in the system.
-// //			The PHP file returns 1 or 0 if the user exists in the system or not, respectively.
-// //*************************************************************************************************
-// function checkUserExists(name){
-// 	var xhttp = new XMLHttpRequest();
-	
-// 	var boolExists = false;
-	
-// 	if (window.location.href.includes('PHP'))
-// 	{
-// 		xhttp.open("POST", "../../scripts/PHP/userExistsCheck.php", true);
-// 	}
-// 	else
-// 	{
-// 		xhttp.open("POST", "scripts/PHP/userExistsCheck.php", true);
-// 	}
-	
-
-// 	// onreadystatechange to get the result!
-// 	// Use callbacks to get value of boolExists back?
-	
-// 	xhttp.onreadystatechange = function(){
-// 		if(xhttp.readyState == 4 && this.status == 200){
-// 	        var exists = xhttp.responseText;
-// 	        boolExists = parseInt(exists);
-// 	    }
-// 		return boolExists;
-// 	};
-	
-// 	alert("After callback");
-	
-// 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-// 	xhttp.send("allowedUser=" + name);// send stuff
-	
-
-// }
-
-
 
 
 //************************************************************************************************
@@ -114,7 +78,7 @@ function openConfirmAddUser(name, roomid, beingEdited)
 //			or not. If we do, it will call addUserWhitelist.php, which actually adds the user
 //			to the whitelist.
 //*************************************************************************************************
-function addWL(name, roomid){
+function addWL(name, roomid, saveObject){
 	// xhttp send is structured like this:
 		// xhttp.send("variable=" + variable + "&variable2=" + variable2 + ...)
 		
@@ -158,7 +122,7 @@ function addWL(name, roomid){
 	        	}
 	        }
 	        else{
-        	    var buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='window.location.reload()'>Ok</button>";
+        	    var buttonhtml = "<br> <br><button class = 'modal-button btn btn-success' id='yesAddWL' onclick='window.location.reload(); saveChanges(saveObject)'>Ok</button>";
 	        	showMessageBox("", "Success!", buttonhtml, false);
 	        }
 	        
@@ -185,7 +149,10 @@ function addWL(name, roomid){
 //
 // Purpose: Saves the changes made to the room. Called in openConfirmAddUser if the user hits yes.
 //*************************************************************************************************
-function saveChanges(roomid, roomType, floorNum, seats, numComputers, limit, beingEdited, hasComputers){
+function saveChanges(saveObject){
+	
+	// roomid, roomType, floorNum, seats, numComputers, limit, beingEdited, hasComputers
+	
 	var xhttp = new XMLHttpRequest();
 
 	if (window.location.href.includes('PHP'))
@@ -199,12 +166,24 @@ function saveChanges(roomid, roomType, floorNum, seats, numComputers, limit, bei
 	
 	xhttp.onreadystatechange = function(){
 		if(xhttp.readyState == 4 && this.status == 200){
-	        window.location.reload();
+	        
 		}
 	};
 	
 	
+	// var saveObject = {
+	// 	numComp : numComputers,
+	// 	hasComp : hasComputers,
+	// 	lim : limit,
+	// 	seatNum : seats,
+	// 	floor : floorNum,
+	// 	roomT : roomType,
+	//  roomID : roomid,
+	//  edit : beingEdited
+	// };
+	
+	
 	
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xhttp.send("roomid=" + roomid + "&roomType=" + roomType + "&floorNum=" + floorNum + "&seats=" + seats + "&numComputers=" + numComputers + "&limit=" + limit + "&beingEdited=" + beingEdited + "&hasComputers=" + hasComputers);// send stuff
+	xhttp.send("roomid=" + saveObject.roomID + "&roomType=" + saveObject.roomT + "&floorNum=" + saveObject.floor + "&seats=" + saveObject.seatNum + "&numComputers=" + saveObject.numComp + "&limit=" + saveObject.lim + "&beingEdited=" + saveObject.edit + "&hasComputers=" + saveObject.hasComp);// send stuff
 }
