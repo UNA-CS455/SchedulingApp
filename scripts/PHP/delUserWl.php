f850c0917b27b6c72b1be038e1fe26d41191dfcb
Hayden Cornelius <haydenc52@gmail.com>
2019-03-20 17:28:33 +0000

    Testing the <select> form field for our delete

--- scripts/PHP/delUserWl.php
+++ scripts/PHP/delUserWl.php
@@ -16,7 +16,8 @@ else{ // Else, echo out the email to limitUser.js
     echo "Successfully deleted $email";
 }
 
-echo "<br> hello " + $email + " in " + $roomid;
+// echo "<br> hello " + $email + " in " + $roomid;
+echo $_POST['allowedUser'] + " " + $_POST['roomid'];
 
 $conn->close();
 ?>
