bd1bd6ecc22ee0ae4ecef30c23ef4a6a22773135
Hayden Cornelius <haydenc52@gmail.com>
2019-03-20 16:43:13 +0000

    Testing the <select> form field for our delete

--- scripts/PHP/createEditRoom.php
+++ scripts/PHP/createEditRoom.php
@@ -245,16 +245,20 @@ function saveChanges($conn, $beingEdited, $post_vars){
                     <div class="col-md-1 form-group" style="min-width: 190px; margin-left: 45px; visibility: hidden">
                     	<b>Spacer, shouldn't be seen</b>
 					</div>
-					<div class = "col-md-3">
+					<div class = "col-md-3" style="padding-left: 27px; padding-top: 10px; padding-right: 15px; padding-bottom: 5px">
 						<!--Delete button here?-->
-						<select name="cars">
+						<select name="cars" style="min-width: 170px">
 						  <option value="volvo">Volvo</option>
 						  <option value="saab">Saab</option>
 						  <option value="fiat">Fiat</option>
 						  <option value="audi">Audi</option>
 						</select>
 					</div>
-					
+					<div class="col-xl-3" >
+							<button class="btn btn-secondary" style="margin-top: 55px; margin-left: 50px; <?php echo ($roomToEdit['limit'] == 1) ? "display: run-in" : "display: none"?>" 
+								id="delUser" name="delUser" type="button">
+								Delete User</button>
+					</div>
 					
 				</div>
 				
