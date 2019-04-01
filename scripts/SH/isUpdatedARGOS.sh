88cda4701f70e6c9e32d45554596256b9d29fc96
Hayden Cornelius <haydenc52@gmail.com>
2019-04-01 23:52:49 +0000

    Testing out .sh scripting

--- scripts/SH/isUpdatedARGOS.sh
+++ scripts/SH/isUpdatedARGOS.sh
@@ -3,7 +3,7 @@
 file1="/2018Fall_course_info1.csv"
 file2="/2018Fall_course_info2.csv"
 
-if cmp -s "$file1" "$file2"; then
+if cmp -s "$file1" "$file1"; then
     printf 'The file "%s" is the same as "%s"\n' "$file1" "$file2"
 else
     printf 'The file "%s" is different from "%s"\n' "$file1" "$file2"
