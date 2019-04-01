d53a6ec05cedd60e45115904e09966fe23bfa0c7
Hayden Cornelius <haydenc52@gmail.com>
2019-04-01 22:59:13 +0000

    Testing out .sh scripting

--- scripts/SH/isUpdatedARGOS.sh
+++ scripts/SH/isUpdatedARGOS.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cmp -s 2018Fall_course_info1.csv 2018Fall_course_info2.csv;
+cmp -s 2018Fall_course_info1.csv 2018Fall_course_info2.csv && echo 'Files are the same' || echo 'Files are different';
 
 sleep 5s;
