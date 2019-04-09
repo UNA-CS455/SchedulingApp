f0ef40d3f8dc88b27365a50f7f56144de7694814
Hayden Cornelius <haydenc52@gmail.com>
2019-04-09 13:36:07 +0000

    Testing if we need to sort both times on comm

--- scripts/SH/isUpdatedARGOS.sh
+++ scripts/SH/isUpdatedARGOS.sh
@@ -21,7 +21,7 @@ unique_to_file1="C:/xampp/htdocs/SchedulingApp/unique_to_file1.csv"
 unique_to_file2="C:/xampp/htdocs/SchedulingApp/unique_to_file2.csv"
 
 comm -2 -3 <(sort $file1) <(sort $file2) > $unique_to_file1
-comm -1 -3 <(sort $file1) <(sort $file2) > $unique_to_file2
+comm -1 -3 $file1 $file2 > $unique_to_file2
 
 
 #We will need to make a file/function to handle the case where we need to export the differences in the new file to another file
