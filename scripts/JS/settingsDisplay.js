a23c135e111b8299f61e609d2dcc93db903c2ed9
Hayden Cornelius <haydenc52@gmail.com>
2019-04-09 16:05:57 +0000

    Added ARGOSautoImportSettings button to the sidebar and functionality

--- scripts/JS/settingsDisplay.js
+++ scripts/JS/settingsDisplay.js
@@ -3,12 +3,14 @@ $(document).ready(function()
   $("#displayUserSettings").show();
   $("#displayRoomSettings").hide();
   $("#displayGroupSettings").hide();
+  $("#ARGOSautoImportSettings").hide();
 
   $("#userSettingsBtn").click(function()
   {
     $("#displayUserSettings").show();
     $("#displayRoomSettings").hide();
     $("#displayGroupSettings").hide();
+    $("#ARGOSautoImportSettings").hide();
   });
 
   $("#roomSettingsBtn").click(function()
@@ -16,6 +18,7 @@ $(document).ready(function()
     $("#displayUserSettings").hide();
     $("#displayRoomSettings").show();
     $("#displayGroupSettings").hide();
+    $("#ARGOSautoImportSettings").hide();
   });
 
   $("#groupSettingsBtn").click(function()
@@ -23,6 +26,15 @@ $(document).ready(function()
     $("#displayUserSettings").hide();
     $("#displayRoomSettings").hide();
     $("#displayGroupSettings").show();
+    $("#ARGOSautoImportSettings").hide();
+  });
+  
+  $("#autoImportSettingsBtn").click(function()
+  {
+    $("#displayUserSettings").hide();
+    $("#displayRoomSettings").hide();
+    $("#displayGroupSettings").hide();
+    $("#ARGOSautoImportSettings").show();
   });
 
 })
