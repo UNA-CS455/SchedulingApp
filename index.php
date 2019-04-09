e1ca6e7396775c0f2fba708629b511c60fc086d4
Hayden Cornelius <haydenc52@gmail.com>
2019-04-09 22:42:36 +0000

    Testing if we can get rid of /schedulingapp

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
     
     <script src="scripts/JS/jquery-3.3.1.min.js"></script>
     <?php 
-      include("{$_SERVER['DOCUMENT_ROOT']}/SchedulingApp/includes/header.php");
+      include("{$_SERVER['DOCUMENT_ROOT']}/includes/header.php");
     ?>
     <div style="position:absolute" id="agendaReservations">
       <!--
