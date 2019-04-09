3bd8215e8a0cb99985a661ca79cfef8e6abaab84
Hayden Cornelius <haydenc52@gmail.com>
2019-04-09 14:08:48 +0000

    Initial commit of EditAutoImport

--- scripts/SH/isUpdatedARGOS.sh
+++ scripts/SH/isUpdatedARGOS.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-file01="C:/xampp/htdocs/SchedulingApp/2018Fall_course_info1.csv"
-file02="C:/xampp/htdocs/SchedulingApp/2018Fall_course_info2.csv"
+oldFile="C:/xampp/htdocs/SchedulingApp/2018Fall_course_info1.csv"
+newFile="C:/xampp/htdocs/SchedulingApp/2018Fall_course_info2.csv"
 
 #cmp may not be the thing we want to use, as we need to get what the differences actually are
     #and actually update the database based on what is new
@@ -17,12 +17,12 @@ file02="C:/xampp/htdocs/SchedulingApp/2018Fall_course_info2.csv"
 
 #comm -2 -3 <(sort file1) <(sort file2) > file3
 
-unique_to_file1="C:/xampp/htdocs/SchedulingApp/unique_to_file1.csv"
-unique_to_file2="C:/xampp/htdocs/SchedulingApp/unique_to_file2.csv"
+unique_to_oldFile="C:/xampp/htdocs/SchedulingApp/unique_to_oldFile.csv"
+unique_to_newFile="C:/xampp/htdocs/SchedulingApp/unique_to_newFile.csv"
 
-comm -2 -3 <(sort $file01) <(sort $file02) > $unique_to_file1
+comm -2 -3 <(sort $oldFile) <(sort $newFile) > $unique_to_oldFile
 # comm -2 -3 $file01 $file02 > $unique_to_file1
-comm -1 -3 <(sort $file01) <(sort $file02) > $unique_to_file2
+comm -1 -3 <(sort $oldFile) <(sort $newFile) > $unique_to_newFile
 
 
 #We will need to make a file/function to handle the case where we need to export the differences in the new file to another file
@@ -32,14 +32,14 @@ comm -1 -3 <(sort $file01) <(sort $file02) > $unique_to_file2
 
 #if [[ -s diff.txt ]]; then echo "file has something"; else echo "file is empty"; fi
 
-if [[ -s $unique_to_file1 ]];
+if [[ -s $unique_to_oldFile ]]; #We have old data that needs to be deleted
 then
-    echo "File has stuff unique to file 1";
+    echo "File has stuff unique to the old file";
 fi
     
-if [[ -s $unique_to_file2 ]]
+if [[ -s $unique_to_newFile ]]; #We have new data that needs to be inserted
 then
-    echo "File has stuff unique to file 2";
+    echo "File has stuff unique to the new file";
 else
     echo "File has no stuff :(";
 fi
