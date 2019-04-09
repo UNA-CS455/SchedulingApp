6833cd701b8e50e25f4405e1a412343d02bbd105
Hayden Cornelius <haydenc52@gmail.com>
2019-04-09 13:42:29 +0000

    Testing if we don't sort what happens

--- scripts/SH/isUpdatedARGOS.sh
+++ scripts/SH/isUpdatedARGOS.sh
@@ -20,7 +20,8 @@ file02="C:/xampp/htdocs/SchedulingApp/2018Fall_course_info2.csv"
 unique_to_file1="C:/xampp/htdocs/SchedulingApp/unique_to_file1.csv"
 unique_to_file2="C:/xampp/htdocs/SchedulingApp/unique_to_file2.csv"
 
-comm -2 -3 <(sort $file01) <(sort $file02) > $unique_to_file1
+# comm -2 -3 <(sort $file01) <(sort $file02) > $unique_to_file1
+comm -2 -3 $file01 $file02 > $unique_to_file1
 comm -1 -3 $file01 $file02 > $unique_to_file2
 
 
