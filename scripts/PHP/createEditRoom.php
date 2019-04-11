342918555a179897f09e252e10cfa95419e823e1
Hayden Cornelius <haydenc52@gmail.com>
2019-04-11 15:00:54 +0000

    Added a placeholder for the input in the limit reservations box

--- scripts/PHP/createEditRoom.php
+++ scripts/PHP/createEditRoom.php
@@ -230,7 +230,7 @@ function saveChanges($conn, $beingEdited, $post_vars){
 						<input class="form-check-input" name="limit" id="limitCheck" type="checkbox" <?php echo($roomToEdit['limit'] == 1) ? 'checked value="1"' : '' ?>>
 						<label for="limit">Limit Reservations</label>
 						<div class="row-md-1" id="email"  style="max-width: 175px; margin-top: 23px; <?php echo ($roomToEdit['limit'] == 1) ? "display: block" : "display: none"?>">
-								<input type="text" id="allowedUser" name="allowedUser" class="form-control">
+								<input type="text" id="allowedUser" name="allowedUser" class="form-control" placeholder="Enter an email address">
 						</div>
 					</div>
 					<div class="col-xl-3" >
