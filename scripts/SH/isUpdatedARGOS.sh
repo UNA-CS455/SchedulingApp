ed80ca80cfac02e71549e4c4d9a4ab1c45441dfc
Hayden Cornelius <haydenc52@gmail.com>
2019-04-22 23:17:55 +0000

    Updated calling functions

--- scripts/SH/isUpdatedARGOS.sh
+++ scripts/SH/isUpdatedARGOS.sh
@@ -67,7 +67,7 @@ then
     
     # Need the following in the header of the uniqueDelete.csv files for associative array headers
     echo '"Term Code","Term Description","Full/Part Term Description","Course CRN","Course Subject","Course Number","Course Sequence Number","Building Name","Room Number","Course Start Time","Course End Time","Course Start Date","Course End Date","Sunday Indicator","Monday Indicator","Tuesday Indicator","Wednesday Indicator","Thursday Indicator","Friday Indicator","Saturday Indicator","Course Maximum Enrollment","Course Enrollment"' > $deleteCSV;
-    echo $unique_to_old >> $deleteCSV
+    cat $unique_to_old >> $deleteCSV
     
     
     # ----------------------------------------------------------------
